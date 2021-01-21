988689edbf6f7718e1b3c280aae20f7e126ccd20
cchaimai <77094673+cchaimai@users.noreply.github.com>
2021-01-21 14:47:12 +0700

    Update Cinema.java

--- lect03/src/cinema/Cinema.java
+++ lect03/src/cinema/Cinema.java
@@ -1,13 +1,13 @@
 /************************************************************
- * Student ID: 
- * First Name:
- * Last Name
+ * Student ID: 6300300925
+ * First Name: Samita
+ * Last Name: Srinarang
  *
  * Team:
- * 1. <Student ID> <First Name> <Last Name>
- * 2.
- * 3.
- * 
+ * 1.6330300372นายธนพล ชาวคูเวียง
+ * 2.6330300542นางสาวปิยนุช ทองเอียด
+ * 3.6330300925 samita srinarang
+ *
  *
  *
  ************************************************************/
@@ -17,37 +17,75 @@ package cinema;
 
 import java.util.Scanner;
 
-public class Cinema {
+public class Cinema2 {
 
-    final static Scanner scanner = new Scanner(System.in);    
+    final static Scanner scanner = new Scanner(System.in);
     private int numRow;
     private int numSeat;
     private char[][] cinema;
 
-    Cinema(int numRow, int numSeat) {
+    Cinema2(int numRow, int numSeat) {
         this.numRow = numRow;
         this.numSeat = numSeat;
+        }
+
+    void showCinema() {
+        System.out.println("Cinema: ");
+        System.out.print(" ");
+        for (int n=1 ;n <= numSeat;n++){
+            System.out.print(" "+n);
+        }
+        System.out.println();
+
+        char[][] seat =new char[numRow][numSeat];
+        for (int n=0;n < numRow;n++){
+            for (int m = 0;m < numSeat;m++){
+                seat[n][m] = 'S';
+            }
+        }
+        for (int n = 1;n < numRow;n++){
+            System.out.print(n+1+" ");
+            for(int m = 0;m<numSeat;m++){
+                System.out.print(seat[n][m]+" ");
+            }
+            System.out.println();
+        }
+        System.out.print("S: หมายถึง ว่าง\nB: หมายถึง ซื้อแล้ว");
+        System.out.println();
+    }
+
+    void buyTicket() {
+        System.out.println("Enter a row number:");
+        showPrompt();
+        int numRow = scanner.nextInt();
+        System.out.println("Enter a seat number in that row:");
+        showPrompt();
+        int seatPerRow = scanner.nextInt();
+
+        int numseat = numRow * seatPerRow;
+        int money;
+
+        if (numseat<60){
+            money = numseat * 100;
+        }
+        else{
+            int half1 = numRow/2;
+            int half2 = numRow - half1;
+            money = (half1 * seatPerRow * 100)+(half2*seatPerRow*80);
+        }
+        System.out.println("Ticket: "+money + " บาท");
+    }
+
+    void showStatistics(){
         // TODO
     }
 
-    void showCinema() {
-       // TODO 
-    }
-    
-    void buyTicket() {
-       // TODO
-    }
-
-    void showStatistics() {
-       // TODO
-    }
-        
     static void showMenu() {
         System.out.println();
         System.out.println("1. Show the seats");
         System.out.println("2. Buy a ticket");
         System.out.println("3. Statistics");
-        System.out.println("0. Exit");                    
+        System.out.println("0. Exit");
         System.out.println();
     }
 
@@ -62,8 +100,8 @@ public class Cinema {
         System.out.println("Enter the number of seats in each row:");
         showPrompt();
         int numSeat = scanner.nextInt();
-        
-        Cinema cinema = new Cinema(numRow, numSeat);
+
+        Cinema2 cinema = new Cinema2(numRow, numSeat);
         int choice;
         boolean end = false;
         while (!end && true) {
@@ -72,7 +110,7 @@ public class Cinema {
             choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                    cinema.showCinema();                
+                    cinema.showCinema();
                     break;
                 case 2:
                     cinema.buyTicket();
@@ -86,7 +124,7 @@ public class Cinema {
                 default:
                     break;
             }
-        }        
-        
+        }
+
     }
 }
